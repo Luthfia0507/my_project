1a3b40181a061abffe8c976d78c6b3b2fb25f8f4
Luthfia0507 <luthfia.250180084@mhs.unimal.ac.id>
2026-09-18 12:37:01 +0700

    Add files via upload

---
+++ products.php
@@ -0,0 +1,38 @@
+<?php
+$products = [
+    ["id" => "BRG 001", 
+    "nama" => "Glad2Glow Cushion", 
+    "kategori" => "Make-up",
+    "harga" => 70000,
+    "stok" => 2,
+    "deskripsi" => "Perfect Cover Cushion"],
+
+    ["id" => "BRG 002", 
+    "nama" => "Glowsophy Moisturizer",
+    "kategori" => "Skincare",
+    "harga" => 40000, 
+    "stok" => 12,
+    "deskripsi" => "Watermelon Brightening Moisturizer"],
+
+    ["id" => "BRG 003", 
+    "nama" => "Deazzle Me Setting Spray", 
+    "kategori" => "Make-up",
+    "harga" => 37000,
+    "stok" => 12,
+    "deskripsi" => "Makeup setting spray dewy fix"],
+
+    ["id" => "BRG 004", 
+    "nama" => "Wardah Sunscreen", 
+    "kategori" => "skincare",
+    "harga" => 35000,
+    "stok" => 24,
+    "deskripsi" => "UV SHIELD Bright-c hydrating sunscreen serum SPF 35 PA+++"],
+
+    ["id" => "BRG 005", 
+    "nama" => "Hanasui Lip Balm", 
+    "kategori" => "Make-up",
+    "harga" => 35000,
+    "stok" => 6,
+    "deskripsi" => "next level butter balm tint"],
+];
+?>
